c2411f971a5c3032399d843a9aac11a1ce2b7b83
Julio Cesar <juliowork.1309@gmail.com>
2025-10-07 19:28:55 +0000

    cierre de sesion

--- src/front/components/Private.jsx
+++ src/front/components/Private.jsx
@@ -1,11 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 export const Private = () => {
-    return(
+
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+
+        localStorage.removeItem("token"); // elimina el token
+        navigate("/"); // redirige a la página pública
+    };
+    return (
 
         <div className="row justify-content-center">
-            <div className="col-md-3 mt-5">
-            <img src="https://cdn.memegenerator.es/imagenes/memes/full/31/71/31711211.jpg" alt="profile"
-            className="rounded-circle  mb-3" style={{width: "250px", height: "250px"}} />
+            <div className="col-md-3 mt-3 d-flex justify-content-center">
+                <img src="https://cdn.memegenerator.es/imagenes/memes/full/31/71/31711211.jpg" alt="profile"
+                    className="rounded-circle  mb-3" style={{ width: "250px", height: "250px" }} />
             </div>
+
+            <div className="d-flex justify-content-center">
+                <button className="btn btn-danger" onClick={handleLogout}>
+                    Cerrar sesión
+                </button>
+            </div>
+
         </div>
     )
 }
